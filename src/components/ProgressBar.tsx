240db263a5259a32094255ecf1e36e2bb28c71b2
Guilherme Alexandre Rocha Pereira <guilherme_alexandree@hotmail.com>
2023-01-22 21:50:50 -0300

    feat: adiciona componente ProgressBar

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from 'react'
+import { View } from 'react-native'
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated'
+
+type ProgressBarProps = {
+  progress?: number
+}
+ 
+export function ProgressBar({ progress = 0 }: ProgressBarProps) {
+  const sharedProgress = useSharedValue(progress)
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%` 
+    }
+  })
+
+  useEffect(() => {
+  sharedProgress.value = withTiming(progress)
+  }, [progress])
+
+  return (  
+    <View
+      className='w-full h-3 rounded-xl bg-zinc-700 mt-4'
+    >
+      <Animated.View
+        className='h-3 rounded-xl bg-violet-600'
+        style={style}
+      />
+    </View>
+  )
+}
